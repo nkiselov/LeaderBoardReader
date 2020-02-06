2de8b2b101e96056d78ecc183beaa662cab7fb4e
nikita <NikitaKiselov>
2020-02-06 13:18:47 -0500

    Go over players to get all items

--- src/Runner.java
+++ src/Runner.java
@@ -1,12 +1,25 @@
 import LeaderboardReader.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Runner {
     public static void main(String[] args) {
         //Read data from the leader board
         Player[] players = LeaderBoard.read();
-        //Print data
-        System.out.println(Arrays.toString(players));
+        //Get all possible names
+        ArrayList<String> names = new ArrayList<>();
+        for (int p=0; p<players.length; p++){
+            Item[] items = players[p].itemsHas;
+            if(items==null){
+                continue;
+            }
+            for (int i=0; i<items.length; i++){
+                if(!names.contains(items[i].name)){
+                    names.add(items[i].name);
+                }
+            }
+        }
+        System.out.println(names);
     }
 }
